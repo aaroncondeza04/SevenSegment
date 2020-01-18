f97c94c39fe8241efda2dba551cf45af8fc6c69c
aaroncondeza04 <54989995+aaroncondeza04@users.noreply.github.com>
2020-01-18 18:22:55 +0800

    Add files via upload

---
+++ SevenSegment/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(179, 314)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(108, 35)
+        Me.Button2.TabIndex = 27
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(72, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 35)
+        Me.Button1.TabIndex = 26
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.DimGray
+        Me.Label5.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label5.Location = New System.Drawing.Point(127, 192)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 71)
+        Me.Label5.TabIndex = 25
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.DimGray
+        Me.Label4.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label4.Location = New System.Drawing.Point(141, 263)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 15)
+        Me.Label4.TabIndex = 24
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label7.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label7.Location = New System.Drawing.Point(141, 177)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(81, 15)
+        Me.Label7.TabIndex = 23
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.DimGray
+        Me.Label3.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label3.Location = New System.Drawing.Point(221, 192)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 71)
+        Me.Label3.TabIndex = 22
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.DimGray
+        Me.Label6.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label6.Location = New System.Drawing.Point(127, 106)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(17, 71)
+        Me.Label6.TabIndex = 21
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.DimGray
+        Me.Label2.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label2.Location = New System.Drawing.Point(221, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 71)
+        Me.Label2.TabIndex = 20
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.DimGray
+        Me.Label1.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.Label1.Location = New System.Drawing.Point(141, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 15)
+        Me.Label1.TabIndex = 19
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label8.Location = New System.Drawing.Point(174, 37)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(48, 23)
+        Me.Label8.TabIndex = 28
+        Me.Label8.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(364, 432)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label8)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button2 As Windows.Forms.Button
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents Label5 As Windows.Forms.Label
+    Friend WithEvents Label4 As Windows.Forms.Label
+    Friend WithEvents Label7 As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+    Friend WithEvents Label6 As Windows.Forms.Label
+    Friend WithEvents Label2 As Windows.Forms.Label
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents Timer1 As Windows.Forms.Timer
+    Friend WithEvents Label8 As Windows.Forms.Label
+End Class
